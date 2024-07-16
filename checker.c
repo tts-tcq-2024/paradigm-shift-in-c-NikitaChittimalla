2499d4091c4f285694f232595e0afed2f2029327
NikitaChittimalla <120707107+NikitaChittimalla@users.noreply.github.com>
2024-07-16 11:47:48 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
  
-int is_temperature_ok(float temperature) {
+int is_temperature_ok(float temperature) { //is_temperature_ok renamed to TemparatureinRange or Temperature
     return (temperature >= 0 && temperature <= 45);
 }
  
-int is_soc_ok(float soc) {
+int is_soc_ok(float soc) { //is_soc_ok renamed to socinrange
     return (soc >= 20 && soc <= 80);
 }
  
-int is_charge_rate_ok(float chargeRate) {
+int is_charge_rate_ok(float chargeRate) { //is_charge_rate_ok renamed to chargerateinrange 
     return (chargeRate <= 0.8);
 }
  
 //Aim to check battery Health using is_battery_ok function
 
-int is_battery_ok(float temperature, float soc, float chargeRate) {
+int is_battery_ok(float temperature, float soc, float chargeRate) { //is_battery_ok renamed to BatteryHealth
     if (is_temperature_ok(temperature) && is_soc_ok(soc) && is_charge_rate_ok(chargeRate))
     {
         return 1;
